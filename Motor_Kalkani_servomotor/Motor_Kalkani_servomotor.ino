0f062b646a7afb5679df24ddb5ddc1245d4b6cad
Mehmet Akif Ocak <35992580+maocak@users.noreply.github.com>
2023-02-19 17:37:31 +0300

    Add files via upload

---
+++ Motor_Kalkani_servomotor/Motor_Kalkani_servomotor.ino
@@ -0,0 +1,22 @@
+#include <AFMotor.h>
+#include <Servo.h>   // Kütüphane eklenir. 
+Servo servomotor;    // servomotor isimli bir servo üretilir.
+
+int servopozisyon = 0 ;
+
+void setup() {
+servomotor.attach (10);  // Servo motor pin tanımlanır. 
+}
+
+void loop() {
+for (servopozisyon = 0; servopozisyon <=180; servopozisyon +=1){
+
+servomotor.write(servopozisyon);
+delay(10);
+}
+
+for (servopozisyon=180; servopozisyon >= 0; servopozisyon -=1){
+servomotor.write(servopozisyon);
+delay(10);
+}
+}
